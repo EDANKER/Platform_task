4711a848f1480c3dadb7e2bd824c5b900febe7d6
Эдгар Киракосян <99076691+EDANKER@users.noreply.github.com>
2023-06-01 11:45:47 +0500

    Update Construct.cs

--- task4/Construct.cs
+++ task4/Construct.cs
@@ -4,21 +4,21 @@ public class Construct
 {
     public string? Id { get; set; }
     public string? Platform { get; set; }
-    public List<Targetlist>? Target { get; set; }
-    public Typelist? Type { get; set; }
-
-    public Construct(string? id, string? platform, List<Targetlist>? target, Typelist? type)
+    public Typelist Type { get; set; }
+    public List<Targetlist> Target { get; set; }
+    public Construct(string? id, string? platform, Typelist type, List<Targetlist> target)
     {
         Id = id;
         Platform = platform;
         Type = type;
         Target = target;
     }
+    
 }
 
 public class Targetlist
 {
-    public string? TargetSpisok { get; set; }
+    public string TargetSpisok { get; set; }
 
     public Targetlist(string targetSpisok)
     {
@@ -27,11 +27,12 @@ public class Targetlist
 }
 
 public class Typelist
-{
+{ 
     public string? TypeSpisok { get; set; }
 
-    public Typelist(string typeSpisok)
+    public Typelist(string? typeSpisok)
     {
         TypeSpisok = typeSpisok;
     }
+    
 }
